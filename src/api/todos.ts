d3b68196b3858d3b888a7b6ac03f5ade46fd4344
Odil Abdulloyev <abdulloyevodil@gmail.com>
2025-11-03 18:05:13 +0500

    chore: delete unused import

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,4 +1,4 @@
-import { queryOptions, useMutation, useQuery } from "@tanstack/react-query";
+import { queryOptions, useMutation } from "@tanstack/react-query";
 import { request } from "./request";
 
 export type Todo = {
